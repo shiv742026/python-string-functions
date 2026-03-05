7081ad5ef393acb9b033b625c8169d4221267742
shiv742026 <sagarvp74@gmail.com>
2026-03-05 22:46:36 +0530

    functions

---
+++ string functions.py
@@ -0,0 +1,69 @@
+#x="programming language"
+#print(x.upper())
+
+
+#x="PROGRAMMING LANGUAGE"
+#print(x.lower())
+
+
+#x="programming"
+#print(x.capitalize())  #this will make first letter of a single word as capital letter
+
+
+#x="programming language"
+#print(x.title())       #first letter of every word will be capital
+
+
+#x="PROGRAMMING"
+#print(x.casefold())      #doubt d/f b/t lower and it
+
+
+#x="hello"
+#print(x.encode())      #doubt its purpose and its working
+
+
+#x="hello world"
+#print(x.swapcase())    #it will swap from upper to lower and vice versa
+
+
+#x="python is a programming language"
+#print(x.replace("python is a","C++ is also a"))       #replace a word or edit a sentence
+
+
+#x= "shivashankar"
+#print(x.find("a"))      # used to find a letter or a word
+
+
+
+#x="shivashankar"
+#print(x.count("a"))    #used to count a letter or word
+
+
+#x="hello world"
+#print(x.index("d"))   #used to know the position of a letter in a word
+
+
+#x="hello,world"
+#print(x.split("   ,   "))
+
+
+#x="        programming"
+#print(x.strip())       #irrdicates whitespace
+
+
+
+#x="shivashankar"
+#print(x.center(50))     #to print at center
+
+
+
+#x="program"
+#print(x.expandtabs())
+
+
+#x="programming"
+#print(x.isalpha())       #to check alphabet or not
+
+
+#x="python123"
+#print(x.isalnum())       #to check for both alphabet and number
